f255539ca513d5b61437f2cd29a82368ead9e07d
Chunqing Hao <helenahcq@yahoo.com>
2022-07-08 22:35:33 +0200

    port

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import os
 import datetime
-from flask import Flask, render_template, request, redirect, session, url_for, abort
+from flask import Flask, render_template, request, redirect, session, url_for, Blueprint
 from werkzeug.utils import secure_filename
 from xml.dom.minidom import parse
 from graphviz import Digraph
 from alphaminer import test_data
 from handle_xes import handle_test
-
-
+# from alphaminer import s_TITO,s_NoTITO,g,TI, TO,TL ,Digraph,YL
 hai = Blueprint("hai", __name__, url_prefix="/ports/9012")
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 # @app.route("/index", methods = ["GET", "POST"])
 @app.route("/index", methods = ["GET"])
@@ -27,7 +26,9 @@ def hello_world():
 		return redirect(url_for("index"), code=302)
 	return render_template("index.html", image=image)
 
-def parse_xes_file(path):	
+def parse_xes_file(path):
+    # dom1 = parse('webservice/upload_folder/L1.xes')
+	
     dom1 = parse(path)
 
     traces = dom1.getElementsByTagName("trace")
@@ -47,17 +48,19 @@ def parse_xes_file(path):
     return L
 
 def parse_data(data):
+	# TODO 在这里处理解析出来的数据生成图片后
+	# 并保存到static目录下,并将图片名返回
 	return test_data(data)
 
-@app.route("/image/<image>", methods = ["GET"])
+# @app.route("/image/<image>", methods = ["GET"])
+@hai.route("/image/<image>", methods = ["GET"])
 def image(image):
-	image = "/static/{}".format(image)
+	image = "ports/9012/static/{}".format(image)
 	print(image)
 	return render_template("index.html", image=image)
 
-
-@hai.route("/upload", methods = ["POST"])
 # @app.route("/upload", methods = ["POST"])
+@hai.route("/upload", methods = ["POST"])
 def upload():
 	image=None
 	file = request.files['file']
@@ -70,9 +73,9 @@ def upload():
 
 		image = parse_data(lll)
 		print(lll)
-	return redirect(url_for("image", image=image), code=302)
-	
-	
+	# return redirect(url_for(".ports/image", image=image), code=302)
+	return render_template("index.html", image=image)
+
 app.register_blueprint(hai)
 
 @app.route('/about')
